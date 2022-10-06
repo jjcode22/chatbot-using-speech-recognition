eb10e55fcce39aba30d12429428c3ad58d01b080
jjmcode <108716229+jjmcode@users.noreply.github.com>
2022-10-06 14:52:57 +0530

    Delete chatbot directory

--- chatbot/microbots/covid19/config.py
+++
@@ -1,4 +0,0 @@
-
-# covid19_api from Postman
-# a summary of new and total cases per country updated daily
-endPointUrl = 'https://api.covid19api.com/summary'
--- chatbot/setup.py
+++
@@ -1,5 +0,0 @@
-import setuptools
-
-setuptools.setup(
-    setup_requires=['pbr>=1.8'],
-    pbr=True)
