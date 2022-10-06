361298fc5a0ca39ab4f62e4268d5ae3a80c11430
jjmcode <108716229+jjmcode@users.noreply.github.com>
2022-10-06 14:47:32 +0530

    Add files via upload
    first commit

---
+++ chatbot/config.py
@@ -0,0 +1,9 @@
+google_api_key = "AIzaSyAbPuAB2pK5vuIiwEJYppTOLxd6w6_3ywU"  # add config key
+weather_api_key = "162195a8a76076b20a151f99c716b638" # add config key
+
+
+# adding domains for intent classification
+domains = ['music', 'restaurant', 'weather']
+
+geolocation_api = "https://geolocation-db.com/json"
+weather_temperature_format = "metric"  # It gives temperature in celsius
